a59d5781ca6d3c7fbcaf79081e73d51eaa3f36d0
Brian Emilius <brian@brianemilius.com>
2021-09-02 14:47:03 +0200

    add cookie support

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,28 @@
-const CACHE_NAME = "setbook_cache_v1";
+const CACHE_NAME = "setbook_cache_v1"
 var urlsToCache = [
-	"/"
-];
+  "/"
+]
 
-self.addEventListener("install", function(event) {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-			.then(function(cache) {
-				console.log("cache opened");
-				return cache.addAll(urlsToCache);
-			})
-	);
-});
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log("cache opened")
+        return cache.addAll(urlsToCache)
+      })
+  )
+})
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         // Cache hit - return response
         if (response) {
-          return response;
+          return response
         }
-        return fetch(event.request);
+        return fetch(event.request)
       }
-    )
-  );
-});
+      )
+  )
+})
--- src/App.js
+++ src/App.js
@@ -4,28 +4,37 @@ import RunSet from "./views/RunSet"
 import CreateUser from "./views/CreateUser"
 import LogIn from "./views/LogIn"
 import TokenContext from "./contexts/TokenContext"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Home from "./views/Home"
+import getCookie from "./helpers/get-cookie"
 
 export default function App() {
 	var tokenState = useState(null)
 
+	useEffect(function() {
+		var cookie = getCookie("sb-token")
+		if (cookie === "") return
+
+		var setToken = tokenState[1];
+		setToken(cookie)
+	}, [])
+
 	return (
 		<div className="App">
 			<TokenContext.Provider value={tokenState}>
-					<Router>
-						{(function () {
-							if (tokenState[0] !== null) {
-								return (<>
-									<Home path="/home" />
-									<RunSet path="/runset/:exercise" />
-								</>)
-							}
-						})()}
-						<LogIn default path="/login" />
-						<CreateUser path="/create-user" />
-					</Router>
+				<Router>
+					{(function () {
+						if (tokenState[0] !== null) {
+							return (<>
+								<Home path="/home" />
+								<RunSet path="/runset/:exercise" />
+							</>)
+						}
+					})()}
+					<LogIn default path="/login" />
+					<CreateUser path="/create-user" />
+				</Router>
 			</TokenContext.Provider>
 		</div>
-	);
+	)
 }
