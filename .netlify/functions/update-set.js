42ca99541b7fcdf54b6ab74256b2916a9cb6b7c9
Brian Emilius <brian@brianemilius.com>
2021-09-29 12:34:49 +0200

    add save and update plus remove set and button block

--- .netlify/functions/update-set.js
+++ .netlify/functions/update-set.js
@@ -2,17 +2,17 @@ var validToken = require("../validateToken")
 var connect = require("../mongodb")
 const { ObjectId } = require("mongodb")
 
-async function saveSet(db, body) {
-	var result = await db.collection("sets").insertOne({
-		user: ObjectId(body.user),
-		exercise: ObjectId(body.exercise),
-		date: body.date,
-		sets: body.sets
-	})
+async function updateSet(db, id, body) {
+	var result = await db.collection("sets").updateOne(
+		{_id: ObjectId(id)},{
+			$set: {
+				sets: body.sets
+			}
+		})
 
 	return {
-		statusCode: 201,
-		body: JSON.stringify({insertedId: result.insertedId})
+		statusCode: 204,
+		body: JSON.stringify(result[0])
 	}
 }
 
@@ -20,11 +20,11 @@ module.exports.handler = async function(event, context) {
 	context.callbackWaitsForEmptyEventLoop = false
 
 	// Check HTTP Method
-	if (event.httpMethod !== "POST") return {
+	if (event.httpMethod !== "PATCH") return {
 		statusCode: 405,
 		body: "METHOD NOT ALLOWED",
 		headers: {
-			"Allow": "POST"
+			"Allow": "PATCH"
 		}
 	}
 
@@ -42,6 +42,7 @@ module.exports.handler = async function(event, context) {
 
 	var body = JSON.parse(event.body)
 	var db = await connect()
+	var id = event.queryStringParameters.id
 	
-	return saveSet(db, body)
+	return updateSet(db, id, body)
 }
--- src/views/RunSet.js
+++ src/views/RunSet.js
@@ -1,22 +1,78 @@
-import { Button, Typography, Container } from "@material-ui/core"
-import { useState, useEffect, useContext } from "react"
+import { Button, Typography, Container, Snackbar } from "@material-ui/core"
+import { SaveAlt } from "@material-ui/icons"
+import { useState, useEffect, useContext, useRef } from "react"
 import Record from "../components/Record"
 import Timer from "react-compound-timer"
 import axios from "axios"
 import AppBar from "../components/AppBar"
 import TokenContext from "../contexts/TokenContext"
 import SetTable from "../components/SetTable"
+import Spinner from "../components/Spinner"
 import "./RunSet.scss"
 
 export default function Home({ exercise }) {
+	var [token] = useContext(TokenContext)
 	var [inputFields, setInputFields] = useState([])
 	var [running, setRunning] = useState(false)
 	var [exerciseObject, setExercise] = useState([])
 	var [prevSet, setPrevSet] = useState({})
-	var [token] = useContext(TokenContext)
+	var [started, setStarted] = useState(false)
+	var [isLoading, setIsLoading] = useState(true)
+	var [open, setOpen] = useState(false)
+	var [snackbarMessage, setSnackbarMessage] = useState("")
+	var [isSaved, setIsSaved] = useState(false)
+	var [setId, setSetId] = useState(null)
+	var [disabled, setDisabled] = useState(false)
+
+	function saveSet(data) {
+		axios.post("/.netlify/functions/save-set", JSON.stringify(data), {
+			headers: {
+				authorization: token
+			}
+		})
+			.then(response => {
+				if (response.status === 201) {
+					setSnackbarMessage("Sets saved")
+					setSetId(response.data.insertedId)
+					setOpen(true)
+					setIsSaved(true)
+				} else {
+					setSnackbarMessage("Something went wrong")
+					setOpen(true)
+				}
+			})
+			.catch(error => {
+				console.error(error)
+				setSnackbarMessage("Something went wrong")
+				setOpen(true)
+			})
+	}
+
+	function updateSet(data) {
+		axios.patch(`/.netlify/functions/update-set?id=${setId}`, JSON.stringify(data), {
+			headers: {
+				authorization: token
+			}
+		})
+			.then(response => {
+				if (response.status === 204) {
+					setSnackbarMessage("Sets updated")
+					setOpen(true)
+				} else {
+					setSnackbarMessage("Something went wrong")
+					setOpen(true)
+				}
+			})
+			.catch(error => {
+				console.error(error)
+				setSnackbarMessage("Something went wrong")
+				setOpen(true)
+			})
+	}
 
 	function handleSubmit(e) {
 		e.preventDefault()
+
 		var data = {
 			user: JSON.parse(token.split("~")[1]).data.id,
 			date: Date.now(),
@@ -24,18 +80,26 @@ export default function Home({ exercise }) {
 			sets: inputFields
 		}
 
-		axios.post("/.netlify/functions/save-set", JSON.stringify(data), {
-			headers: {
-				authorization: token
-			}
-		})
-			.then(response => console.log(response))
+		isSaved ? updateSet(data) : saveSet(data)
+	}
+
+	function handleClose() {
+		setOpen(false)
 	}
 
 	function addSet() {
 		var values = [...inputFields, { start: Date.now(), end: 0, reps: 0, weight: 0 }]
 		setInputFields(values)
 		setRunning(true)
+		setStarted(true)
+	}
+
+	function removeSet(index) {
+		if (running) return
+
+		var values = [...inputFields]
+		values.splice(index, 1)
+		setInputFields(values)
 	}
 
 	function stopSet() {
@@ -46,6 +110,12 @@ export default function Home({ exercise }) {
 	}
 
 	function resetTimer() {
+		setDisabled(true)
+
+		setTimeout(function() {
+			setDisabled(false)
+		}, 3000)
+		
 		if (!running) {
 			addSet()
 			return
@@ -74,38 +144,50 @@ export default function Home({ exercise }) {
 				authorization: token
 			}
 		})
-			.then(res => setPrevSet(res.data))
+			.then(res => {
+				setPrevSet(res.data)
+				setIsLoading(false)
+			})
 	}, [exercise, token])
-
 
 	return (
 		<>
 			<AppBar back="/home" />
 			<Container className="viewContainer">
-				<form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", padding: "1em" }}>
+				{isLoading ? <Spinner /> : <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", justifyContent: "space-between", padding: "1em" }}>
 					<Typography className="text" variant="h5" component="h1">{exerciseObject.title}</Typography>
+					
 					{prevSet.sets ?
 						<SetTable date={prevSet.date} sets={prevSet.sets} />
 						: <Typography className="text">You have not done this exercise before</Typography>}
-					<div style={{ display: "flex", overflowX: "scroll", margin: "1em 0", minHeight: "96px" }}>
+
+					<div style={{ display: "flex", overflowX: "scroll", margin: "1em 0", minHeight: "162px" }}>
 						{inputFields.map((inputField, index) => (
-							<Record inputField={inputField} index={index} key={index} inputFields={inputFields} setInputFields={setInputFields} />
+							<Record inputField={inputField} index={index} key={index} inputFields={inputFields} setInputFields={setInputFields} removeSet={removeSet} />
 						))}
 					</div>
+
 					<Timer timeToUpdate="10" onReset={resetTimer}>
 						{({ reset }) => (
 							<>
-								{!running && <Button variant="contained" className="masterButton masterButton--start" size="large" type="button" onClick={reset}>Start</Button>}
-								{running && <Button type="button" className="masterButton masterButton--end" variant="contained" size="large" onClick={reset}>Stop</Button>}
+							<Button
+								variant="contained"
+								size="large"
+								type="button"
+								onClick={reset}
+								className={"masterButton masterButton--" + (running ? "end" : "start") + (disabled ? " disabled" : "")}>
+									{running ? "Stop" : "Start"}
+								</Button>
 								<div className={"timer" + (!running ? " timer--running" : "")}>
-									<Timer.Minutes formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`} />:<Timer.Seconds formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`} />
+									{!started ? null : <><Timer.Minutes formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`} />:<Timer.Seconds formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`} /></>}
 								</div>
 							</>
 						)}
 					</Timer>
-					<Button type="submit">Save</Button>
-				</form>
+					<Button type="submit" variant="contained" endIcon={<SaveAlt />}>Save</Button>
+				</form>}
 			</Container>
+			<Snackbar open={open} message={snackbarMessage} autoHideDuration={5000} onClose={handleClose} />
 		</>
 	)
 }
